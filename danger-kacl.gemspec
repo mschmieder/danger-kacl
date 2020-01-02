6328afee278afba45e7960c05ce103c406524cdb
Matthias Schmieder <matthias.schmieder@stryker.com>
2020-01-02 14:57:39 +0100

    initial commit

--- danger-kacl.gemspec
+++ danger-kacl.gemspec
@@ -10,7 +10,7 @@ Gem::Specification.new do |spec|
   spec.email         = ['matthias.schmieder@stryker.com']
   spec.description   = %q{A short description of danger-kacl.}
   spec.summary       = %q{A longer description of danger-kacl.}
-  spec.homepage      = 'https://github.com/Matthias Schmieder/danger-kacl'
+  spec.homepage      = 'https://github.com/mschmieder/danger-kacl'
   spec.license       = 'MIT'
 
   spec.files         = `git ls-files`.split($/)
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_runtime_dependency 'danger-plugin-api', '~> 1.0'
+  spec.add_runtime_dependency 'json', '~> 2.3'
 
   # General ruby development
   spec.add_development_dependency 'bundler', '~> 1.3'
--- lib/kacl/plugin.rb
+++ lib/kacl/plugin.rb
@@ -1,3 +1,6 @@
+require 'fileutils'
+require 'json'
+
 module Danger
   # This is your plugin class. Any attributes or methods you expose here will
   # be available from within your Dangerfile.
@@ -17,17 +20,56 @@ module Danger
   # @tags monday, weekends, time, rattata
   #
   class DangerKacl < Plugin
+    ERROR_FILE_NOT_SET = "kacl report file not set. Use 'kacl_changelog.report = \"kacl_report.json\"'.".freeze
+    ERROR_FILE_NOT_FOUND = "No file found at %s".freeze
 
-    # An attribute that you can read/write from your Dangerfile
-    #
-    # @return   [Array<String>]
-    attr_accessor :my_attribute
+    attr_accessor :kacl_report_file
 
-    # A method that you can call from your Dangerfile
-    # @return   [Array<String>]
+    def kacl_cli_installed?
+      `which kacl-cli`.strip.empty? == false
+    end
+
+    def kacl_report_file
+      @kacl_report_file || "kacl_report.json"
+    end
+
+
+    def is_valid
+      valid = kacl_report['valid']
+      if valid
+        message  "Changelog validity is '%s'" % [valid]
+      else
+        errors = kacl_report['errors']
+
+        for e in errors do
+          start_char_pos = 0
+          if e.key?("start_char_pos") and e['start_char_pos'] != nil
+            start_char_pos = e['start_char_pos']
+          end
+          fail "CHANGELOG:#{e['line_number']}:#{start_char_pos} error: #{e['error_message']}"
+        end
+      end
+
+    end
+
+    private
+
+    # Convenient method to not always parse the task file but keep it in the memory.
     #
-    def warn_on_mondays
-      warn 'Trying to merge code on a Monday' if Date.today.wday == 1
+    # @return [IniFile::Hash] The task report object.
+    def parse_kacl_report_file
+      raise ERROR_FILE_NOT_SET if kacl_report_file.nil? || kacl_report_file.empty?
+      raise format(ERROR_FILE_NOT_FOUND, kacl_report_file) unless File.exist?(kacl_report_file)
+
+      file = File.read(kacl_report_file)
+      JSON.parse(file)
+    end
+
+    # Convenient method to not always parse the task file but keep it in the memory.
+    #
+    # @return [IniFile::Hash] The task report object.
+    def kacl_report
+      @kacl_report ||= parse_kacl_report_file
     end
   end
 end
